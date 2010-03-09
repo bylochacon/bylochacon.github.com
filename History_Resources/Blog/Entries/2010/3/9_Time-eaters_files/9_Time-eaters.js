f7a7d3db70e58d058817ce6a3b7258f06d8189dc
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-03-09 12:57:43 -0800

    added blog, formatted half of womens links

---
+++ History_Resources/Blog/Entries/2010/3/9_Time-eaters_files/9_Time-eaters.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20100309
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,200),url:'9_Time-eaters_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'9_Time-eaters_files/stroke_1.png'},{rect:new IWRect(5,-5,385,10),url:'9_Time-eaters_files/stroke_2.png'},{rect:new IWRect(390,-5,10,10),url:'9_Time-eaters_files/stroke_3.png'},{rect:new IWRect(390,5,10,200),url:'9_Time-eaters_files/stroke_4.png'},{rect:new IWRect(390,205,10,10),url:'9_Time-eaters_files/stroke_5.png'},{rect:new IWRect(5,205,385,10),url:'9_Time-eaters_files/stroke_6.png'},{rect:new IWRect(-5,205,10,10),url:'9_Time-eaters_files/stroke_7.png'}],new IWSize(395,210))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('9_Time-eaters_files/9_Time-eatersMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id8');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
