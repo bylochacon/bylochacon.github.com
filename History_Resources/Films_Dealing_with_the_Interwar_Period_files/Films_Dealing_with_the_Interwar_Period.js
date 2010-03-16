5c34a112ae06595875a460ef04106a54bae49e2a
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-03-16 13:57:07 -0700

    more womens links formatting

--- History_Resources/Films_Dealing_with_the_Interwar_Period_files/Films_Dealing_with_the_Interwar_Period.js
+++ History_Resources/Films_Dealing_with_the_Interwar_Period_files/Films_Dealing_with_the_Interwar_Period.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100315
+// Created by iWeb 2.0.4 local-build-20100316
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
