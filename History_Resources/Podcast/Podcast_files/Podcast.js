92acfe3e1f3dd52d92af21e0d3481bcba5d96ee1
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-03-25 23:52:24 -0700

    reformatted links for misc and groups

--- History_Resources/Podcast/Podcast_files/Podcast.js
+++ History_Resources/Podcast/Podcast_files/Podcast.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100320
+// Created by iWeb 2.0.4 local-build-20100325
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
