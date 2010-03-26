c4afcc9d1e88e7cd719054a81ff17acfc09fc177
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-03-26 11:09:22 -0700

    formatted group links

--- History_Resources/Groups,_Sharing_&_Networking:__YouTube_files/Groups,_Sharing_&_Networking:__YouTube.js
+++ History_Resources/Groups,_Sharing_&_Networking:__YouTube_files/Groups,_Sharing_&_Networking:__YouTube.js
@@ -1,11 +1,11 @@
-// Created by iWeb 2.0.4 local-build-20100325
+// Created by iWeb 2.0.4 local-build-20100326
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),stroke_1:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Groups,_Sharing_&_Networking:__YouTube_files/Groups,_Sharing_&_Networking:__YouTubeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');detectBrowser();adjustLineHeightIfTooBig('id12');adjustFontSizeIfTooBig('id12');adjustLineHeightIfTooBig('id13');adjustFontSizeIfTooBig('id13');Widget.onload();applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');Widget.onload();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
--- History_Resources/Podcast/Entries/2010/3/14_Rwandan_Genocide:__An_overview_files/14_Rwandan_Genocide:__An_overview.js
+++ History_Resources/Podcast/Entries/2010/3/14_Rwandan_Genocide:__An_overview_files/14_Rwandan_Genocide:__An_overview.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100325
+// Created by iWeb 2.0.4 local-build-20100326
 
 setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
