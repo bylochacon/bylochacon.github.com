ee5e5107f1e91f38e0f213e52961c4863e580102
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-04-26 20:34:11 -0700

    moved

--- Megans_Site/Megans_Watches_files/Megans_Watches.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 2.0.4 local-build-20100420
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Megans_Watches_files/Megans_WatchesMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');Widget.onload();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
