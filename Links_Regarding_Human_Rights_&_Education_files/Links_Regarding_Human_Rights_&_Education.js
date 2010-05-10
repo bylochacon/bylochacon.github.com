923d91e0cb6775bfd13f2b246c4cf0a2e4aad121
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-05-10 11:59:14 -0700

    blog and teaching bib

--- Links_Regarding_Human_Rights_&_Education_files/Links_Regarding_Human_Rights_&_Education.js
+++ Links_Regarding_Human_Rights_&_Education_files/Links_Regarding_Human_Rights_&_Education.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100505
+// Created by iWeb 2.0.4 local-build-20100510
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
--- Podcast/Archive_files/Archive.js
+++ Podcast/Archive_files/Archive.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100505
+// Created by iWeb 2.0.4 local-build-20100510
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
