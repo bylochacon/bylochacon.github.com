c1abd0e07620af87bc6c6cc1743a3d713289fbd8
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-05-20 17:25:51 -0700

    hate definitions and blog

--- Films_Dealing_with_the_Armenian_Genocide_files/Films_Dealing_with_the_Armenian_Genocide.js
+++ Films_Dealing_with_the_Armenian_Genocide_files/Films_Dealing_with_the_Armenian_Genocide.js
@@ -1,7 +1,7 @@
 // Created by iWeb 2.0.4 local-build-20100520
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke(),stroke_5:new IWEmptyStroke(),stroke_4:new IWEmptyStroke(),stroke_6:new IWEmptyStroke(),stroke_3:new IWEmptyStroke(),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000}),stroke_2:new IWEmptyStroke()});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000}),stroke_5:new IWEmptyStroke(),stroke_6:new IWEmptyStroke(),stroke_0:new IWEmptyStroke(),stroke_4:new IWEmptyStroke(),stroke_1:new IWEmptyStroke(),stroke_3:new IWEmptyStroke(),stroke_2:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
