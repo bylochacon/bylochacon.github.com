d5edadd8a835c93f2a173aa29d8dcb29ffc36dd2
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-06-29 11:24:29 -0700

    added presentations and some site cleanup

--- Definitions_of_Hate_Issues_files/Definitions_of_Hate_Issues.js
+++ Definitions_of_Hate_Issues_files/Definitions_of_Hate_Issues.js
@@ -1,7 +1,7 @@
-// Created by iWeb 2.0.4 local-build-20100623
+// Created by iWeb 2.0.4 local-build-20100629
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),shadow_4:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_5:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_3:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_2:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_5:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_3:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_4:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),stroke_0:new IWEmptyStroke(),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
