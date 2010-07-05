8d0de4b76af1556a095a89087dabc8743bae8188
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-07-05 11:35:50 -0700

    blog and projects links and formatting

--- -!-/Entries/2010/3/14_Rwandan_Genocide_files/14_Rwandan_Genocide.js
+++ -!-/Entries/2010/3/14_Rwandan_Genocide_files/14_Rwandan_Genocide.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100629
+// Created by iWeb 2.0.4 local-build-20100705
 
 setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
