cc53c5181743d67ad7b6d8236da6b472d95d470b
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-07-12 13:26:54 -0700

    blog and killing definitions

--- Links_by_Geographic_&_Cultural_Areas_files/Links_by_Geographic_&_Cultural_Areas.js
+++ Links_by_Geographic_&_Cultural_Areas_files/Links_by_Geographic_&_Cultural_Areas.js
@@ -1,7 +1,7 @@
-// Created by iWeb 2.0.4 local-build-20100707
+// Created by iWeb 2.0.4 local-build-20100712
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_10:new IWEmptyStroke(),stroke_2:new IWEmptyStroke(),stroke_3:new IWEmptyStroke(),stroke_9:new IWEmptyStroke(),stroke_6:new IWEmptyStroke(),stroke_11:new IWEmptyStroke(),stroke_12:new IWEmptyStroke(),stroke_4:new IWEmptyStroke(),stroke_8:new IWEmptyStroke(),stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke(),stroke_5:new IWEmptyStroke(),stroke_13:new IWEmptyStroke(),stroke_7:new IWEmptyStroke()});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_11:new IWEmptyStroke(),stroke_1:new IWEmptyStroke(),stroke_2:new IWEmptyStroke(),stroke_6:new IWEmptyStroke(),stroke_8:new IWEmptyStroke(),stroke_7:new IWEmptyStroke(),stroke_3:new IWEmptyStroke(),stroke_4:new IWEmptyStroke(),stroke_9:new IWEmptyStroke(),stroke_5:new IWEmptyStroke(),stroke_0:new IWEmptyStroke(),stroke_12:new IWEmptyStroke(),stroke_13:new IWEmptyStroke(),stroke_10:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
