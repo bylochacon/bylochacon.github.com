6f3b86cd952ae5621d175553f9e46b2a85b05a4c
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-07-15 10:41:38 -0700

    blog and journals and downloads

--- Bibliography_of_the_Indonesian_Massacres_files/Bibliography_of_the_Indonesian_Massacres.js
+++ Bibliography_of_the_Indonesian_Massacres_files/Bibliography_of_the_Indonesian_Massacres.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100712
+// Created by iWeb 2.0.4 local-build-20100715
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWStrokeParts([{rect:new IWRect(-1,1,2,266),url:'Bibliography_of_the_Indonesian_Massacres_files/stroke.png'},{rect:new IWRect(-1,-1,2,2),url:'Bibliography_of_the_Indonesian_Massacres_files/stroke_1.png'},{rect:new IWRect(1,-1,141,2),url:'Bibliography_of_the_Indonesian_Massacres_files/stroke_2.png'},{rect:new IWRect(142,-1,2,2),url:'Bibliography_of_the_Indonesian_Massacres_files/stroke_3.png'},{rect:new IWRect(142,1,2,266),url:'Bibliography_of_the_Indonesian_Massacres_files/stroke_4.png'},{rect:new IWRect(142,267,2,2),url:'Bibliography_of_the_Indonesian_Massacres_files/stroke_5.png'},{rect:new IWRect(1,267,141,2),url:'Bibliography_of_the_Indonesian_Massacres_files/stroke_6.png'},{rect:new IWRect(-1,267,2,2),url:'Bibliography_of_the_Indonesian_Massacres_files/stroke_7.png'}],new IWSize(143,268)),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
--- Downloads_files/Downloads.js
+++ Downloads_files/Downloads.js
@@ -1,11 +1,11 @@
-// Created by iWeb 2.0.4 local-build-20100531
+// Created by iWeb 2.0.4 local-build-20100715
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),shadow_1:new IWShadow({blurRadius:12,offset:new IWPoint(5.6569,5.6569),color:'#008080',opacity:0.750000}),stroke_1:new IWEmptyStroke(),shadow_0:new IWShadow({blurRadius:12,offset:new IWPoint(5.6569,5.6569),color:'#008080',opacity:0.750000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Downloads_files/DownloadsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');detectBrowser();Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');detectBrowser();adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
