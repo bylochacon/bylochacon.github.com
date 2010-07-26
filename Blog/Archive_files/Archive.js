b3f02e1c0197b259a4cf31826460c959b6464bac
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-07-25 19:44:11 -0700

    east timor links and blog

--- Blog/Archive_files/Archive.js
+++ Blog/Archive_files/Archive.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100722
+// Created by iWeb 2.0.4 local-build-20100725
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
--- Links_Regarding_the_Afro-Caribbean_World_files/Links_Regarding_the_Afro-Caribbean_World.js
+++ Links_Regarding_the_Afro-Caribbean_World_files/Links_Regarding_the_Afro-Caribbean_World.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100722
+// Created by iWeb 2.0.4 local-build-20100725
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Links_Regarding_the_Afro-Caribbean_World_files/Links_Regarding_the_Afro-Caribbean_WorldMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id4');fixupIECSS3Opacity('id6');fixupIECSS3Opacity('id8');fixupIECSS3Opacity('id10');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');detectBrowser();adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
