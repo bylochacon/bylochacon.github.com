bc09e34cceb9a5641d46a2eebc93c896a290fd62
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-08-18 11:32:04 -0700

    Indonesian Massacres topic blog and links

--- Imperialism_files/Imperialism.js
+++ Imperialism_files/Imperialism.js
@@ -1,7 +1,7 @@
-// Created by iWeb 2.0.4 local-build-20100730
+// Created by iWeb 2.0.4 local-build-20100818
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-4,4,8,142),url:'Imperialism_files/stroke.png'},{rect:new IWRect(-4,-4,8,8),url:'Imperialism_files/stroke_1.png'},{rect:new IWRect(4,-4,140,8),url:'Imperialism_files/stroke_2.png'},{rect:new IWRect(144,-4,8,8),url:'Imperialism_files/stroke_3.png'},{rect:new IWRect(144,4,8,142),url:'Imperialism_files/stroke_4.png'},{rect:new IWRect(144,146,8,8),url:'Imperialism_files/stroke_5.png'},{rect:new IWRect(4,146,140,8),url:'Imperialism_files/stroke_6.png'},{rect:new IWRect(-4,146,8,8),url:'Imperialism_files/stroke_7.png'}],new IWSize(148,150)),stroke_1:new IWEmptyStroke()});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWStrokeParts([{rect:new IWRect(-4,4,8,142),url:'Imperialism_files/stroke.png'},{rect:new IWRect(-4,-4,8,8),url:'Imperialism_files/stroke_1.png'},{rect:new IWRect(4,-4,140,8),url:'Imperialism_files/stroke_2.png'},{rect:new IWRect(144,-4,8,8),url:'Imperialism_files/stroke_3.png'},{rect:new IWRect(144,4,8,142),url:'Imperialism_files/stroke_4.png'},{rect:new IWRect(144,146,8,8),url:'Imperialism_files/stroke_5.png'},{rect:new IWRect(4,146,140,8),url:'Imperialism_files/stroke_6.png'},{rect:new IWRect(-4,146,8,8),url:'Imperialism_files/stroke_7.png'}],new IWSize(148,150))});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
