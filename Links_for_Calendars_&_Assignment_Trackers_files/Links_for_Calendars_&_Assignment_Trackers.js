d615778623ba74ef0ff1373d4e208244db698132
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-08-25 13:17:18 -0700

    blog and darfur

--- Links_for_Calendars_&_Assignment_Trackers_files/Links_for_Calendars_&_Assignment_Trackers.js
+++ Links_for_Calendars_&_Assignment_Trackers_files/Links_for_Calendars_&_Assignment_Trackers.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100818
+// Created by iWeb 2.0.4 local-build-20100825
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
