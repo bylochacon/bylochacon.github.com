f9d2000f4659a9b30d2829799fffcf94e411f6ff
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-09-07 12:00:16 -0700

    burundi links

--- Links_for_Hate_Issues_files/Links_for_Hate_Issues.js
+++ Links_for_Hate_Issues_files/Links_for_Hate_Issues.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100901
+// Created by iWeb 2.0.4 local-build-20100907
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
--- Links_for_University_World_History_&_Genocide_Studies_Departments_files/Links_for_University_World_History_&_Genocide_Studies_Departments.js
+++ Links_for_University_World_History_&_Genocide_Studies_Departments_files/Links_for_University_World_History_&_Genocide_Studies_Departments.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100901
+// Created by iWeb 2.0.4 local-build-20100907
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
