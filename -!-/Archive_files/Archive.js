8ff28081e58be0ec601192cccbfd91aa509287e8
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-09-10 11:22:12 -0700

    yugoslavia links

--- -!-/Archive_files/Archive.js
+++ -!-/Archive_files/Archive.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100909
+// Created by iWeb 2.0.4 local-build-20100910
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
--- Groups,_Sharing_&_Networking:__LinkedIn_files/Groups,_Sharing_&_Networking:__LinkedIn.js
+++ Groups,_Sharing_&_Networking:__LinkedIn_files/Groups,_Sharing_&_Networking:__LinkedIn.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100909
+// Created by iWeb 2.0.4 local-build-20100910
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_7:new IWEmptyStroke(),stroke_1:new IWEmptyStroke(),stroke_5:new IWEmptyStroke(),stroke_0:new IWEmptyStroke(),stroke_2:new IWEmptyStroke(),stroke_3:new IWEmptyStroke(),stroke_4:new IWEmptyStroke(),stroke_6:new IWEmptyStroke()});registry.applyEffects();}
