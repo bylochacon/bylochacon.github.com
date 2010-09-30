abaf8220e810d418635ff1bd91ab571b04c75444
Jessica Bylo Chacon <bylochacon@gmail.com>
2010-09-29 21:00:44 -0700

    added definition links to all topic pages

--- Links_for_Websites_by_Teachers_files/Links_for_Websites_by_Teachers.js
+++ Links_for_Websites_by_Teachers_files/Links_for_Websites_by_Teachers.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20100928
+// Created by iWeb 2.0.4 local-build-20100929
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_2:new IWEmptyStroke(),stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
