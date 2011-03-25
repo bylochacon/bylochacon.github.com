5e0a5c0f08f84d94ea824dc859c1c9a7db61ab2c
Jessica Bylo Chacon <bylochacon@gmail.com>
2011-03-25 11:25:30 -0700

    world war ii overviews bibliography and blog entry

--- Links_for_General_Reference_files/Links_for_General_Reference.js
+++ Links_for_General_Reference_files/Links_for_General_Reference.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20110323
+// Created by iWeb 2.0.4 local-build-20110325
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke(),stroke_2:new IWEmptyStroke()});registry.applyEffects();}
