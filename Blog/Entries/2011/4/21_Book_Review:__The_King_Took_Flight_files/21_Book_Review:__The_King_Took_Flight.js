6ac5e43dee3383445bc058d0c366bb04705b734a
Jessica Bylo Chacon <bylochacon@gmail.com>
2011-04-21 13:02:25 -0700

    tackett review, blog, french revolution overviews books

---
+++ Blog/Entries/2011/4/21_Book_Review:__The_King_Took_Flight_files/21_Book_Review:__The_King_Took_Flight.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20110421
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,200),url:'21_Book_Review%3A__The_King_Took_Flight_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'21_Book_Review%3A__The_King_Took_Flight_files/stroke_1.png'},{rect:new IWRect(5,-5,385,10),url:'21_Book_Review%3A__The_King_Took_Flight_files/stroke_2.png'},{rect:new IWRect(390,-5,10,10),url:'21_Book_Review%3A__The_King_Took_Flight_files/stroke_3.png'},{rect:new IWRect(390,5,10,200),url:'21_Book_Review%3A__The_King_Took_Flight_files/stroke_4.png'},{rect:new IWRect(390,205,10,10),url:'21_Book_Review%3A__The_King_Took_Flight_files/stroke_5.png'},{rect:new IWRect(5,205,385,10),url:'21_Book_Review%3A__The_King_Took_Flight_files/stroke_6.png'},{rect:new IWRect(-5,205,10,10),url:'21_Book_Review%3A__The_King_Took_Flight_files/stroke_7.png'}],new IWSize(395,210))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('21_Book_Review:__The_King_Took_Flight_files/21_Book_Review:__The_King_Took_FlightMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
--- Podcast/Archive_files/Archive.js
+++ Podcast/Archive_files/Archive.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20110419
+// Created by iWeb 2.0.4 local-build-20110421
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
