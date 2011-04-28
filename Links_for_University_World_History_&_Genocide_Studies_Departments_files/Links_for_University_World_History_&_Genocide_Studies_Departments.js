27da5d892a462bf291c8d61ae0c7de241442810f
Jessica Bylo Chacon <bylochacon@gmail.com>
2011-04-28 13:33:56 -0700

    added nazi bibliography and blog

--- Links_for_University_World_History_&_Genocide_Studies_Departments_files/Links_for_University_World_History_&_Genocide_Studies_Departments.js
+++ Links_for_University_World_History_&_Genocide_Studies_Departments_files/Links_for_University_World_History_&_Genocide_Studies_Departments.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20110426
+// Created by iWeb 2.0.4 local-build-20110428
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),stroke_1:new IWEmptyStroke()});registry.applyEffects();}
