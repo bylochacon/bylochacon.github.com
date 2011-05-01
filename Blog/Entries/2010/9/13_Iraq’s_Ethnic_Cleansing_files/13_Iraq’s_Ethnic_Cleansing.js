2eadbae3cc41deb68bce980e010bd3790d8ac7ac
Jessica Bylo Chacon <bylochacon@gmail.com>
2011-05-01 15:31:04 -0700

    totalitarianism bibliography

--- Blog/Entries/2010/9/13_Iraq’s_Ethnic_Cleansing_files/13_Iraq’s_Ethnic_Cleansing.js
+++ Blog/Entries/2010/9/13_Iraq’s_Ethnic_Cleansing_files/13_Iraq’s_Ethnic_Cleansing.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20110428
+// Created by iWeb 2.0.4 local-build-20110501
 
 setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,200),url:'13_Iraq%E2%80%99s_Ethnic_Cleansing_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'13_Iraq%E2%80%99s_Ethnic_Cleansing_files/stroke_1.png'},{rect:new IWRect(5,-5,385,10),url:'13_Iraq%E2%80%99s_Ethnic_Cleansing_files/stroke_2.png'},{rect:new IWRect(390,-5,10,10),url:'13_Iraq%E2%80%99s_Ethnic_Cleansing_files/stroke_3.png'},{rect:new IWRect(390,5,10,200),url:'13_Iraq%E2%80%99s_Ethnic_Cleansing_files/stroke_4.png'},{rect:new IWRect(390,205,10,10),url:'13_Iraq%E2%80%99s_Ethnic_Cleansing_files/stroke_5.png'},{rect:new IWRect(5,205,385,10),url:'13_Iraq%E2%80%99s_Ethnic_Cleansing_files/stroke_6.png'},{rect:new IWRect(-5,205,10,10),url:'13_Iraq%E2%80%99s_Ethnic_Cleansing_files/stroke_7.png'}],new IWSize(395,210))});registry.applyEffects();}
