1ca1ca37053b1fe907e5d1225454648f9a3d58ce
Jessica Bylo Chacon <bylochacon@gmail.com>
2011-05-19 12:52:12 -0700

    american revolution books

---
+++ Blog/Entries/2011/5/19_Overviews_of_the_American_Revolution_files/19_Overviews_of_the_American_Revolution.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20110519
+
+setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-5,5,10,200),url:'19_Overviews_of_the_American_Revolution_files/stroke.png'},{rect:new IWRect(-5,-5,10,10),url:'19_Overviews_of_the_American_Revolution_files/stroke_1.png'},{rect:new IWRect(5,-5,385,10),url:'19_Overviews_of_the_American_Revolution_files/stroke_2.png'},{rect:new IWRect(390,-5,10,10),url:'19_Overviews_of_the_American_Revolution_files/stroke_3.png'},{rect:new IWRect(390,5,10,200),url:'19_Overviews_of_the_American_Revolution_files/stroke_4.png'},{rect:new IWRect(390,205,10,10),url:'19_Overviews_of_the_American_Revolution_files/stroke_5.png'},{rect:new IWRect(5,205,385,10),url:'19_Overviews_of_the_American_Revolution_files/stroke_6.png'},{rect:new IWRect(-5,205,10,10),url:'19_Overviews_of_the_American_Revolution_files/stroke_7.png'}],new IWSize(395,210))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('19_Overviews_of_the_American_Revolution_files/19_Overviews_of_the_American_RevolutionMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
