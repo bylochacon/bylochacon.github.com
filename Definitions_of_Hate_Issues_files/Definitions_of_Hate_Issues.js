ca947977e20e72d957ceb7d1a31330222c427a04
Jessica Bylo Chacon <bylochacon@gmail.com>
2011-06-07 11:40:19 -0700

    economics definitions and blog and note on top of all blog and note on top of all definitions pages

--- Definitions_of_Hate_Issues_files/Definitions_of_Hate_Issues.js
+++ Definitions_of_Hate_Issues_files/Definitions_of_Hate_Issues.js
@@ -1,11 +1,11 @@
-// Created by iWeb 2.0.4 local-build-20110602
+// Created by iWeb 2.0.4 local-build-20110607
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_3:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_4:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_2:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_5:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_5:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_1:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_2:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_3:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000}),shadow_4:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.500000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Definitions_of_Hate_Issues_files/Definitions_of_Hate_IssuesMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');detectBrowser();adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');detectBrowser();adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
--- News_of_Hate_files/News_of_Hate.js
+++ News_of_Hate_files/News_of_Hate.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20110602
+// Created by iWeb 2.0.4 local-build-20110607
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
