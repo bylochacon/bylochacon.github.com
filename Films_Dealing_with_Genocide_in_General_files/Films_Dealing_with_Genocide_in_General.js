d9e1cd4411f3564d0ffc1a4687f597a191074f5b
Jessica Bylo Chacon <bylochacon@gmail.com>
2011-07-19 13:17:05 -0700

    defs blogs armenian genocide jehovahs witness sitjehovahs suggestions

--- Films_Dealing_with_Genocide_in_General_files/Films_Dealing_with_Genocide_in_General.js
+++ Films_Dealing_with_Genocide_in_General_files/Films_Dealing_with_Genocide_in_General.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20110630
+// Created by iWeb 2.0.4 local-build-20110719
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),shadow_0:new IWShadow({blurRadius:4,offset:new IWPoint(0.0000,0.0000),color:'#000000',opacity:0.750000})});registry.applyEffects();}
--- Podcast/Podcast_files/Podcast.js
+++ Podcast/Podcast_files/Podcast.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20110630
+// Created by iWeb 2.0.4 local-build-20110719
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
