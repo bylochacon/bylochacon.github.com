b3a327a0ee64f8e4dc938a137e4767a3ae716126
Jessica Bylo Chacon <bylochacon@gmail.com>
2011-07-23 23:04:56 -0700

    added Holocaust definitions and people

--- Links_for_Textbook_Resources_&_Reviews_files/Links_for_Textbook_Resources_&_Reviews.js
+++ Links_for_Textbook_Resources_&_Reviews_files/Links_for_Textbook_Resources_&_Reviews.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20110719
+// Created by iWeb 2.0.4 local-build-20110723
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
