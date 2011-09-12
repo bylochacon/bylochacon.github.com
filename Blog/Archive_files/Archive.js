120bf11e62930e4b7b935ac8f81783c2bc1ef141
Jessica Bylo Chacon <bylochacon@gmail.com>
2011-09-12 12:09:00 -0700

    downloads setup

--- Blog/Archive_files/Archive.js
+++ Blog/Archive_files/Archive.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20110901
+// Created by iWeb 2.0.4 local-build-20110912
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
---
+++ Downloads_for_World_History:_Unit_2,_Revolutionary_Era_files/Downloads_for_World_History:_Unit_2,_Revolutionary_Era.js
@@ -0,0 +1,11 @@
+// Created by iWeb 2.0.4 local-build-20110912
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Downloads_for_World_History:_Unit_2,_Revolutionary_Era_files/Downloads_for_World_History:_Unit_2,_Revolutionary_EraMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
