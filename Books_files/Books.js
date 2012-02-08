ca7031ac060d8ebdc345a784836767659be9d315
Jessica Bylo Chacon <bylochacon@gmail.com>
2012-02-07 16:29:02 -0800

    added ancient europe bibliography

--- Books_files/Books.js
+++ Books_files/Books.js
@@ -1,11 +1,11 @@
-// Created by iWeb 2.0.4 local-build-20120206
+// Created by iWeb 2.0.4 local-build-20120207
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),stroke_1:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Books_files/BooksMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');adjustLineHeightIfTooBig('id13');adjustFontSizeIfTooBig('id13');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id8');fixupIECSS3Opacity('id10');fixupIECSS3Opacity('id12');fixupIECSS3Opacity('id14');fixupIECSS3Opacity('id15');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id12');adjustFontSizeIfTooBig('id12');Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id9');fixupIECSS3Opacity('id11');fixupIECSS3Opacity('id13');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
