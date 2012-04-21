4e3336a06e0dd3d7322acc8df309ef9f5b5d3a2c
Jessica Bylo Chacon <bylochacon@gmail.com>
2012-04-20 18:41:30 -0700

    added blog and site guide for holo links

--- Blog/Archive_files/Archive.js
+++ Blog/Archive_files/Archive.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20120419
+// Created by iWeb 2.0.4 local-build-20120420
 
 setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
 {return false;}
