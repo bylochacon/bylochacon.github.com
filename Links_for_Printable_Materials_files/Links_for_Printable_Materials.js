385a4741fdcb3735cdc36b37565ec6756ae666df
Jessica Bylo Chacon <bylochacon@gmail.com>
2012-04-23 15:01:53 -0700

    added images to holocaust links and fixed page counter

--- Links_for_Printable_Materials_files/Links_for_Printable_Materials.js
+++ Links_for_Printable_Materials_files/Links_for_Printable_Materials.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20120420
+// Created by iWeb 2.0.4 local-build-20120423
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(1.4142,1.4142),color:'#191919',opacity:0.500000}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
--- Links_for_Thematic_&_Commodity_Histories_files/Links_for_Thematic_&_Commodity_Histories.js
+++ Links_for_Thematic_&_Commodity_Histories_files/Links_for_Thematic_&_Commodity_Histories.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20120420
+// Created by iWeb 2.0.4 local-build-20120423
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
