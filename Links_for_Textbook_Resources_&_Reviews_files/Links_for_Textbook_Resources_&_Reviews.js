e67a9d0f5cacb0aa6ac6173227274ead00307b6b
Jessica Bylo Chacon <bylochacon@gmail.com>
2012-05-06 19:13:35 -0700

    updated herero links

--- Links_for_Textbook_Resources_&_Reviews_files/Links_for_Textbook_Resources_&_Reviews.js
+++ Links_for_Textbook_Resources_&_Reviews_files/Links_for_Textbook_Resources_&_Reviews.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20120504
+// Created by iWeb 2.0.4 local-build-20120506
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_1:new IWEmptyStroke(),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
--- Podcast/Entries/2012/10/14_Introduction_files/14_Introduction.js
+++ Podcast/Entries/2012/10/14_Introduction_files/14_Introduction.js
@@ -1,4 +1,4 @@
-// Created by iWeb 2.0.4 local-build-20120504
+// Created by iWeb 2.0.4 local-build-20120506
 
 setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
